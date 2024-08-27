e1fe128703b8dfb95ff4ccb49af4d37219ff6605
hanapiko <apikojuma94@gmail.com>
2024-08-27 18:42:35 +0300

    modify floor.go to return position

--- day_1/floor/floor.go
+++ day_1/floor/floor.go
@@ -1,15 +1,31 @@
 package floor
 
-func FloorNum(s string) int{
+func FloorNum(s string) int {
 	count := 0
-	for _, ch := range s{
+	for _, ch := range s {
 		if ch == '(' {
 			count++
 			// continue
 		} else {
-		count--
-	}
+			count--
+		}
 	}
 	return count
 }
 
+func Position(s string) int {
+	count := 0
+	pos := 0
+	for i, ch := range s {
+		if ch == '(' {
+			count++
+		} else {
+			count--
+		}
+		if count == -1 {
+			pos = i + 1
+			break
+		}
+	}
+	return pos
+}
