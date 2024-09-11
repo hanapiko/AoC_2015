13aa1bc575b5651c24c64dfbf53091df3729e647
hanapiko <apikojuma94@gmail.com>
2024-09-11 08:48:11 +0300

    split main to functions

--- day_2/main.go
+++ day_2/main.go
@@ -12,13 +12,13 @@ func main() {
     // Open the data.txt file
     file, err := os.Open("data.txt")
     if err != nil {
-        fmt.Fprintf(os.Stderr, "Error opening file")
+        fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
         return
     }
     defer file.Close()
 
     scanner := bufio.NewScanner(file)
-    WrappingPaper := 0
+    wrappingPaper := 0
     ribbon := 0
 
     for scanner.Scan() {
@@ -30,7 +30,7 @@ func main() {
         }
         l, err := strconv.Atoi(dimensions[0])
         if err != nil {
-            fmt.Println("Error parsing")
+            fmt.Println("Error parsing length:", err)
             continue
         }
         w, err := strconv.Atoi(dimensions[1])
@@ -44,26 +44,34 @@ func main() {
             continue
         }
 
-        area1 := l * w
-        area2 := w * h
-        area3 := h * l
-
-        sArea := 2*(area1 + area2 + area3)
-        small:= min(area1, min(area2, area3))
-
-        WrappingPaper += sArea + small
-
-        perimeter1 := 2 * (l + w)
-        perimeter2 := 2 * (w + h)
-        perimeter3 := 2 * (h + l)
-        smallestPerimeter := min(perimeter1, min(perimeter2, perimeter3))
-
-        volume := l * w * h
-        ribbon += smallestPerimeter + volume
+        wrappingPaper += calculateWrappingPaper(l, w, h)
+        ribbon += calculateRibbon(l, w, h)
     }
 
-    fmt.Println(WrappingPaper)
+    fmt.Println(wrappingPaper)
     fmt.Println(ribbon)
+}
+
+func calculateWrappingPaper(l, w, h int) int {
+    area1 := l * w
+    area2 := w * h
+    area3 := h * l
+
+    sArea := 2 * (area1 + area2 + area3)
+    small := min(area1, min(area2, area3))
+
+    return sArea + small
+}
+
+func calculateRibbon(l, w, h int) int {
+    perimeter1 := 2 * (l + w)
+    perimeter2 := 2 * (w + h)
+    perimeter3 := 2 * (h + l)
+
+    smallestPerimeter := min(perimeter1, min(perimeter2, perimeter3))
+    volume := l * w * h
+
+    return smallestPerimeter + volume
 }
 
 // minimum of two integers
@@ -73,4 +81,3 @@ func min(a, b int) int {
     }
     return b
 }
-
